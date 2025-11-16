bbd07049af0c2e6d39a7fae9805c8207eea658a5
Joemar Catipon <joemarcatipon7@gmail.com>
2025-11-17 04:42:54 +0800

    Implements JWT for security

---
+++ utils/jwt.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const createJWT = ({ payload }) => {
+  return jwt.sign(
+    payload, process.env.JWT_SECRET, 
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+}
+
+const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
+
+module.exports = {
+  createJWT,
+  isTokenValid
+}
