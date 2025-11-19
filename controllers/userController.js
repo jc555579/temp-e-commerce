cbd170f4a23eeb38b8c68283cb13c793e2a2c456
Joemar Catipon <joemarcatipon7@gmail.com>
2025-11-19 10:30:51 +0800

    Create user routes. Completed getSingle, all Users, and authorization

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
+const getAllUsers = async (req, res) => {
+  console.log(req.user);
+  const users = await User.find({ role: 'user' }).select('-password');
+
+  res.status(StatusCodes.OK).json({ users });
+};
+
+const getSingleUser = async (req, res) => {
+  const userId = req.params.id;
+  
+  const user = await User.findOne({ _id: userId }).select('-password');
+
+  if (!user) {
+    throw new CustomError.NotFoundError('No user with id: ' + userId);
+  }
+
+  res.status(StatusCodes.OK).json({ user });
+};
+
+const showCurrentUser = async (req, res) => {
+  res.send('show current user');
+};
+
+const updateUser = async (req, res) => {
+  res.send('update user');
+};
+
+const updateUserPassword = async (req, res) => {
+  res.send('update user password');
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword
+};
---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
+
+// controller
+const { getAllUsers, getSingleUser,showCurrentUser,
+  updateUser, updateUserPassword } = require('../controllers/userController');
+
+router.route('/').get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers);
+
+router.route('/showMe').get(showCurrentUser);
+router.route('/updateUser').patch(updateUser);
+router.route('/updateUserPassword').patch(updateUserPassword);
+
+router.route('/:id').get(authenticateUser, getSingleUser);
+
+module.exports = router;
