63c3a7d855c0d85e4e800003ca1d1707825372b9
Joemar Catipon <joemarcatipon7@gmail.com>
2025-11-26 10:10:22 +0800

    Implemented Review model

---
+++ models/Review.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    require: [true, 'Please provide rating number'],
+  },
+  title: {
+    type: String,
+    trim: true,
+    require: [true, 'Please provide title'],
+    maxlength: 100,
+  },
+  comment: {
+    type: String,
+    require: [true, 'Please provide review text'],
+  },  
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: [true, 'Please provide user id'],
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    require: [true, 'Please provide product id'],
+  },
+}, { timestamps: true });
+
+// ensures only one review per product each user
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+module.exports = mongoose.model('Review', ReviewSchema);
